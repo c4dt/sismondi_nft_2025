2e33584ed49ae9f65e7f3f3166169754c0bb23be
Linus Gasser <linus.gasser@epfl.ch>
2023-12-19 15:20:10 +0100

    Use local python and npm if docker is not available

--- brownie.sh
+++ brownie.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run --rm --platform linux/amd64 -v $PWD:/sismondi ghcr.io/c4dt/brownie:latest "$@"
+if docker ps &> /dev/null; then
+  docker run --rm --platform linux/amd64 -v $PWD:/sismondi ghcr.io/c4dt/brownie:latest "$@"
+else
+  brownie "$@"
+fi
--- mk_images.sh
+++ mk_images.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-docker run --rm --platform linux/amd64 -v $PWD:/sismondi \
-  --entrypoint=/usr/local/bin/python ghcr.io/c4dt/brownie:latest scripts/images.py
+if docker ps &> /dev/null; then
+  docker run --rm --platform linux/amd64 -v $PWD:/sismondi \
+    --entrypoint=/usr/local/bin/python ghcr.io/c4dt/brownie:latest scripts/images.py
+else
+  python scripts/images.py
+fi
