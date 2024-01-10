328af0746fc75ca542f1412fdfff1b84367694cf
Linus Gasser <linus.gasser@epfl.ch>
2024-01-10 13:28:15 +0100

    Fix fast_transaction for test network

--- scripts/sismondi.py
+++ scripts/sismondi.py
@@ -1,7 +1,4 @@
 from brownie import SismondiNFT, network, config, accounts, chain
-priority = 5_000_000_000
-network.priority_fee(priority)
-network.max_fee(chain.base_fee + priority)
 
 import logging, sys
 logging.basicConfig(filename='operations.log', encoding='utf-8', level=logging.INFO)
@@ -15,7 +12,13 @@ def log(txt):
     print(txt)
     logging.info(txt)
 
+def fast_transaction():
+    priority = 5_000_000_000
+    network.priority_fee(priority)
+    network.max_fee(chain.base_fee + priority)
+
 def deploy():
+    fast_transaction()
     sismondi_contract = SismondiNFT.deploy(
         {"from": account},
         publish_source=config["networks"][network.show_active()].get("verify")
@@ -24,6 +27,7 @@ def deploy():
     return sismondi_contract
 
 def mint(addr):
+    fast_transaction()
     sismondi_contract = SismondiNFT.at(addr)
     nft = sismondi_contract.makeSismondiNFT({'from': account})
     nft_id = nft.events['NewSismondiNFTMinted']['tokenId']
